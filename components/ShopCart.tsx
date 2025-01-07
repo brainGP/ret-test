386e44c039afa0f5091d2f86c13b7816b73b6825
bata <batorgilerdenbold@gmail.com>
2025-01-07 15:31:41 +0800

    pos of cart text

--- components/ShopCart.tsx
+++ components/ShopCart.tsx
@@ -22,7 +22,7 @@ const ShopCard = () => {
       </div>
 
       {cartTotalQty > 0 && (
-        <div className="absolute top-[-10px] right-[-10px] bg-gray text-white h-5 w-5 rounded-full flex justify-center items-center text-xs">
+        <div className="absolute top-[-10px] right-[-16px] bg-gray text-white h-5 w-5 rounded-full flex justify-center items-center text-xs">
           {cartTotalQty}
         </div>
       )}
